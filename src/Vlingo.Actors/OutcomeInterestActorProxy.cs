70455b4060a997e527d1d473cd5eb20ef2d055c7
Zp Bappi <zpbappi@gmail.com>
2018-05-20 23:39:49 +1000

    implemented OutcomeInteresetActorProxy

--- src/Vlingo.Actors/OutcomeInterestActorProxy.cs
+++ src/Vlingo.Actors/OutcomeInterestActorProxy.cs
@@ -1,24 +1,26 @@
-﻿using System;
-
-namespace Vlingo.Actors
+﻿namespace Vlingo.Actors
 {
     class OutcomeInterestActorProxy<TOutcome, TRef> : IOutcomeInterest<TOutcome>
     {
+        private readonly IOutcomeAware<TOutcome, TRef> outcomeAware;
+        private readonly TRef reference;
+
         public OutcomeInterestActorProxy(
             IOutcomeAware<TOutcome, TRef> outcomeAware,
             TRef reference)
         {
-
+            this.outcomeAware = outcomeAware;
+            this.reference = reference;
         }
 
         public void FailureOutcome(Outcome<TOutcome> outcome)
         {
-            throw new NotImplementedException();
+            outcomeAware.FailureOutcome(outcome, reference);
         }
 
         public void SuccessfulOutcome(Outcome<TOutcome> outcome)
         {
-            throw new NotImplementedException();
+            outcomeAware.SuccessfulOutcome(outcome, reference);
         }
     }
 }
