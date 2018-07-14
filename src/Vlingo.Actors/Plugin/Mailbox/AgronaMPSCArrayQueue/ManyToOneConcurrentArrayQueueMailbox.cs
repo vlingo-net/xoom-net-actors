60ce419bbb5819ccf1a0b8e3aee24d4c6d376d44
Zp Bappi <zpbappi@gmail.com>
2018-07-14 16:40:35 +1000

    implemented mailbox plugins

---
+++ src/Vlingo.Actors/Plugin/Mailbox/AgronaMPSCArrayQueue/ManyToOneConcurrentArrayQueueMailbox.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Concurrent;
+
+namespace Vlingo.Actors.Plugin.Mailbox.AgronaMPSCArrayQueue
+{
+    public class ManyToOneConcurrentArrayQueueMailbox : IMailbox
+    {
+        private readonly IDispatcher dispatcher;
+        private readonly BlockingCollection<IMessage> queue;
+        private readonly int totalSendRetries;
+
+        internal ManyToOneConcurrentArrayQueueMailbox(
+            IDispatcher dispatcher,
+            int mailboxSize,
+            int totalSendRetries)
+        {
+            this.dispatcher = dispatcher;
+            queue = new BlockingCollection<IMessage>(new ConcurrentQueue<IMessage>(), mailboxSize);
+            this.totalSendRetries = totalSendRetries;
+        }
+
+        public void Close()
+        {
+            dispatcher.Close();
+            queue.CompleteAdding();
+            queue.Dispose();
+        }
+
+        public bool IsClosed => dispatcher.IsClosed;
+
+        public bool IsDelivering
+            => throw new NotSupportedException("ManyToOneConcurrentArrayQueueMailbox does not support this operation.");
+
+        public bool Delivering(bool flag) 
+            => throw new NotSupportedException("ManyToOneConcurrentArrayQueueMailbox does not support this operation.");
+
+        public void Run()
+            => throw new NotSupportedException("ManyToOneConcurrentArrayQueueMailbox does not support this operation.");
+
+        public void Send(IMessage message)
+        {
+            for (int tries = 0; tries < totalSendRetries; ++tries)
+            {
+                if (queue.TryAdd(message))
+                {
+                    return;
+                }
+            }
+            throw new InvalidOperationException("Count not enqueue message due to busy mailbox.");
+        }
+
+        public IMessage Receive() => queue.Take();
+    }
+}
---
+++ src/Vlingo.Actors/Plugin/Mailbox/AgronaMPSCArrayQueue/ManyToOneConcurrentArrayQueuePlugin.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Vlingo.Actors.Plugin.Mailbox.AgronaMPSCArrayQueue
+{
+    public class ManyToOneConcurrentArrayQueuePlugin : IPlugin, IMailboxProvider
+    {
+        private ManyToOneConcurrentArrayQueueDispatcherPool dispatcherPool;
+
+        public ManyToOneConcurrentArrayQueuePlugin()
+        {
+        }
+
+        public void Close()
+        {
+            dispatcherPool.Close();
+        }
+
+        public string Name { get; private set; }
+
+        public int Pass => 1;
+
+        public bool IsClosed => throw new System.NotImplementedException();
+
+        public bool IsDelivering => throw new System.NotImplementedException();
+
+        public void Start(IRegistrar registrar, string name, PluginProperties properties)
+        {
+            Name = name;
+            CreateDispatcherPool(properties);
+            RegisterWith(registrar, properties);
+        }
+
+        public IMailbox ProvideMailboxFor(int hashCode) => dispatcherPool.AssignFor(hashCode).Mailbox;
+
+        public IMailbox ProvideMailboxFor(int hashCode, IDispatcher dispatcher) => dispatcherPool.AssignFor(hashCode).Mailbox;
+
+        private void CreateDispatcherPool(PluginProperties properties)
+        {
+            var numberOfDispatchersFactor = properties.GetFloat("numberOfDispatchersFactor", 1.5f);
+            var size = properties.GetInteger("size", 1048576);
+            var fixedBackoff = properties.GetInteger("fixedBackoff", 2);
+            var dispatcherThrottlingCount = properties.GetInteger("dispatcherThrottlingCount", 1);
+            var totalSendRetries = properties.GetInteger("sendRetires", 10);
+
+            dispatcherPool = new ManyToOneConcurrentArrayQueueDispatcherPool(
+                System.Environment.ProcessorCount,
+                numberOfDispatchersFactor,
+                size,
+                fixedBackoff,
+                dispatcherThrottlingCount,
+                totalSendRetries);
+        }
+
+        private void RegisterWith(IRegistrar registrar, PluginProperties properties)
+        {
+            var defaultMailbox = properties.GetBoolean("defaultMailbox", true);
+
+            registrar.Register(Name, defaultMailbox, this);
+        }
+    }
+}
