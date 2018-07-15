572bc5a864040578bf8d6cddb517181b5655578f
Zp Bappi <zpbappi@gmail.com>
2018-07-15 10:43:46 +1000

    added license headers

--- src/Vlingo.Actors/ActorProxy.cs
+++ src/Vlingo.Actors/ActorProxy.cs
@@ -1,4 +1,10 @@
-﻿using System;
+﻿// Copyright (c) 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+using System;
 using Vlingo.Common.Compiler;
 using static Vlingo.Common.Compiler.DynaNaming;
 
--- src/Vlingo.Actors/Plugin/Completes/CompletesEventuallyPool.cs
+++ src/Vlingo.Actors/Plugin/Completes/CompletesEventuallyPool.cs
@@ -1,4 +1,10 @@
-﻿namespace Vlingo.Actors.Plugin.Completes
+﻿// Copyright (c) 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+namespace Vlingo.Actors.Plugin.Completes
 {
     public class CompletesEventuallyPool : ICompletesEventuallyProvider
     {
