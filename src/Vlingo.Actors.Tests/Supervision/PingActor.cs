0c04d1a5d4234965018416decdd0c3b9cd139c59
Zp Bappi <zpbappi@gmail.com>
2018-10-17 20:48:37 +1100

    added tests upto StageSupervisedActorTest

---
+++ src/Vlingo.Actors.Tests/Supervision/PingActor.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System.Threading;
+using Vlingo.Actors.TestKit;
+using Vlingo.Common;
+
+namespace Vlingo.Actors.Tests.Supervision
+{
+    public class PingActor : Actor, IPing
+    {
+        public static ThreadLocal<PingActor> Instance = new ThreadLocal<PingActor>();
+
+        private readonly PingTestResults testResults;
+
+        public PingActor(PingTestResults testResults)
+        {
+            this.testResults = testResults;
+            Instance.Value = this;
+        }
+
+        public void Ping()
+        {
+            testResults.PingCount.IncrementAndGet();
+            testResults.UntilPinged.Happened();
+        }
+
+        public override void Stop()
+        {
+            base.Stop();
+            testResults.UntilStopped.Happened();
+        }
+
+        public class PingTestResults
+        {
+            public AtomicInteger PingCount = new AtomicInteger(0);
+            public TestUntil UntilPinged = TestUntil.Happenings(0);
+            public TestUntil UntilStopped = TestUntil.Happenings(0);
+        }
+    }
+}
