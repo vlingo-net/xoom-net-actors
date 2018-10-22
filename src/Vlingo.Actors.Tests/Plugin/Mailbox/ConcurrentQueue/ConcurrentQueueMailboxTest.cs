2e8bdd33e4e302eea13bcca4cb9de132bf9c1c98
Zp Bappi <zpbappi@gmail.com>
2018-10-22 22:33:40 +1100

    added SharedRingBuffer (with passing tests) and ConcurrentQueue (with failing tests) mailbox plugins

---
+++ src/Vlingo.Actors.Tests/Plugin/Mailbox/ConcurrentQueue/ConcurrentQueueMailboxTest.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) 2012-2018 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.Collections.Generic;
+using Vlingo.Actors.Plugin.Mailbox.ConcurrentQueue;
+using Vlingo.Actors.TestKit;
+using Xunit;
+
+namespace Vlingo.Actors.Tests.Plugin.Mailbox.ConcurrentQueue
+{
+    public class ConcurrentQueueMailboxTest : ActorsTest
+    {
+        private const int Total = 10_000;
+
+        private IDispatcher dispatcher;
+        private IMailbox mailbox;
+
+        public ConcurrentQueueMailboxTest()
+        {
+            dispatcher = new ExecutorDispatcher(1, 1.0f);
+            mailbox = new ConcurrentQueueMailbox(dispatcher, 1);
+        }
+
+        public override void Dispose()
+        {
+            base.Dispose();
+            mailbox.Close();
+            dispatcher.Close();
+        }
+
+        [Fact]
+        public void TestMailboxSendReceive()
+        {
+            var testResults = new TestResults();
+            var actor = new CountTakerActor(testResults);
+            actor.TestResults.Until = Until(Total);
+
+            for (var i = 0; i < Total; ++i)
+            {
+                var countParam = i;
+                Action<ICountTaker> consumer = consumerActor => consumerActor.Take(countParam);
+                var message = new LocalMessage<ICountTaker>(actor, consumer, "Take(int)");
+                mailbox.Send(message);
+            }
+            actor.TestResults.Until.Completes();
+
+            for(var i=0; i<Total; ++i)
+            {
+                Assert.Equal(i, actor.TestResults.Counts[i]);
+            }
+        }
+
+        private class CountTakerActor : Actor, ICountTaker
+        {
+            public CountTakerActor(TestResults testResults)
+            {
+                TestResults = testResults;
+            }
+
+            public TestResults TestResults { get; }
+
+            public void Take(int count)
+            {
+                TestResults.Counts.Add(count);
+                TestResults.Until.Happened();
+            }
+        }
+
+        private class TestResults
+        {
+            public IList<int> Counts = new List<int>();
+            public TestUntil Until = TestUntil.Happenings(0);
+        }
+    }
+}
