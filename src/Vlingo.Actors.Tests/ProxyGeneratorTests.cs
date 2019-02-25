d1941fd43c955b72f7278e3954577e4df2fe56f9
Zp Bappi <zpbappi@gmail.com>
2019-02-25 21:12:23 +1100

    fixed proxy generator to improt namespaces for method parameters

---
+++ src/Vlingo.Actors.Tests/ProxyGeneratorTests.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Xunit;
+
+namespace Vlingo.Actors.Tests
+{
+    public class ProxyGeneratorTests : ActorsTest
+    {
+        [Fact]
+        public void ShouldIncludeNamespacesForMethodParameters()
+        {
+            var generator = ProxyGenerator.ForTest(false, World.DefaultLogger);
+            var result = generator.GenerateFor(typeof(IProxyGenTestInterface));
+            Assert.Contains("using System.Net;", result.Source);
+        }
+    }
+
+    public interface IProxyGenTestInterface
+    {
+        void DoSomething(System.Net.Dns dns);
+    }
+}
