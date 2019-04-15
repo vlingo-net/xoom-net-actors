848164f20710a7026adb5bedf9fd9be8af2dffd3
Zp Bappi <zpbappi@gmail.com>
2019-04-15 14:05:00 +1000

    added addressable

---
+++ src/Vlingo.Actors/Addressable__Proxy.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) 2012-2019 Vaughn Vernon. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+namespace Vlingo.Actors
+{
+    internal class Addressable__Proxy : IAddressable
+    {
+        private readonly Actor actor;
+
+        public Addressable__Proxy(Actor actor, IMailbox mailbox)
+        {
+            this.actor = actor;
+        }
+
+        public IAddress Address => actor.Address;
+
+        public LifeCycle LifeCycle => actor.LifeCycle;
+    }
+}
