a490fd3d256c04a46824363d8c8b4f360d0b6fb1
Zp Bappi <zpbappi@gmail.com>
2019-04-18 14:38:06 +1000

    updated stage test

--- src/Vlingo.Actors.Tests/StageTest.cs
+++ src/Vlingo.Actors.Tests/StageTest.cs
@@ -64,13 +64,12 @@ namespace Vlingo.Actors.Tests
         [Fact]
         public void TestDirectoryScan()
         {
-            var scanFound = new AtomicInteger(0);
-
             var address1 = World.AddressFactory.UniqueWith("test-actor1");
             var address2 = World.AddressFactory.UniqueWith("test-actor2");
             var address3 = World.AddressFactory.UniqueWith("test-actor3");
             var address4 = World.AddressFactory.UniqueWith("test-actor4");
             var address5 = World.AddressFactory.UniqueWith("test-actor5");
+
             var address6 = World.AddressFactory.UniqueWith("test-actor6");
             var address7 = World.AddressFactory.UniqueWith("test-actor7");
 
@@ -80,12 +79,13 @@ namespace Vlingo.Actors.Tests
             World.Stage.Directory.Register(address4, new TestInterfaceActor());
             World.Stage.Directory.Register(address5, new TestInterfaceActor());
 
-            var until = Until(7);
+            var scanResults = new ScanResult();
+            scanResults.AfterCompleting(7);
+
             Action<INoProtocol> afterConsumer = actor =>
             {
                 Assert.NotNull(actor);
-                scanFound.IncrementAndGet();
-                until.Happened();
+                scanResults.ScanFound.WriteUsing("foundContent", 1);
             };
 
             World.Stage.ActorOf<INoProtocol>(address5).AndThenConsume(afterConsumer);
@@ -95,32 +95,87 @@ namespace Vlingo.Actors.Tests
             World.Stage.ActorOf<INoProtocol>(address1).AndThenConsume(afterConsumer);
 
             World.Stage.ActorOf<INoProtocol>(address6)
-                .AndThenConsume(actor =>
+                .AndThen(null, actor =>
                 {
                     Assert.Null(actor);
-                    until.Happened();
+                    scanResults.ScanFound.WriteUsing("foundCount", 1);
+                    return actor;
                 })
-                .Otherwise(actor =>
+                .Otherwise(noSuchActor =>
                 {
-                    Assert.Null(actor);
-                    until.Happened();
+                    Assert.Null(noSuchActor);
+                    scanResults.ScanFound.WriteUsing("notFoundCount", 1);
                     return null;
                 });
             World.Stage.ActorOf<INoProtocol>(address7)
-                .AndThenConsume(actor =>
+                .AndThen(null, actor =>
                 {
                     Assert.Null(actor);
-                    until.Happened();
+                    scanResults.ScanFound.WriteUsing("foundCount", 1);
+                    return actor;
                 })
-                .Otherwise(actor =>
+                .Otherwise(noSuchActor =>
                 {
-                    Assert.Null(actor);
-                    until.Happened();
+                    Assert.Null(noSuchActor);
+                    scanResults.ScanFound.WriteUsing("notFoundCount", 1);
                     return null;
                 });
 
-            until.Completes();
-            Assert.Equal(5, scanFound.Get());
+            Assert.Equal(5, scanResults.ScanFound.ReadFrom<int>("foundCount"));
+            Assert.Equal(2, scanResults.ScanFound.ReadFrom<int>("notFoundCount"));
+        }
+
+        [Fact]
+        public void TestDirectoryScanMaybeActor()
+        {
+            var address1 = World.AddressFactory.UniqueWith("test-actor1");
+            var address2 = World.AddressFactory.UniqueWith("test-actor2");
+            var address3 = World.AddressFactory.UniqueWith("test-actor3");
+            var address4 = World.AddressFactory.UniqueWith("test-actor4");
+            var address5 = World.AddressFactory.UniqueWith("test-actor5");
+
+            var address6 = World.AddressFactory.UniqueWith("test-actor6");
+            var address7 = World.AddressFactory.UniqueWith("test-actor7");
+
+            World.Stage.Directory.Register(address1, new TestInterfaceActor());
+            World.Stage.Directory.Register(address2, new TestInterfaceActor());
+            World.Stage.Directory.Register(address3, new TestInterfaceActor());
+            World.Stage.Directory.Register(address4, new TestInterfaceActor());
+            World.Stage.Directory.Register(address5, new TestInterfaceActor());
+
+            var scanResults = new ScanResult();
+            scanResults.AfterCompleting(7);
+
+            Action<Optional<INoProtocol>> afterConsumer = maybe =>
+            {
+                Assert.True(maybe.IsPresent);
+                scanResults.ScanFound.WriteUsing("foundContent", 1);
+            };
+
+            World.Stage.MaybeActorOf<INoProtocol>(address5).AndThenConsume(afterConsumer);
+            World.Stage.MaybeActorOf<INoProtocol>(address4).AndThenConsume(afterConsumer);
+            World.Stage.MaybeActorOf<INoProtocol>(address3).AndThenConsume(afterConsumer);
+            World.Stage.MaybeActorOf<INoProtocol>(address2).AndThenConsume(afterConsumer);
+            World.Stage.MaybeActorOf<INoProtocol>(address1).AndThenConsume(afterConsumer);
+
+            World.Stage.MaybeActorOf<INoProtocol>(address6)
+                .AndThen(maybe =>
+                {
+                    Assert.False(maybe.IsPresent);
+                    scanResults.ScanFound.WriteUsing("notFoundCount", 1);
+                    return maybe;
+                });
+
+            World.Stage.MaybeActorOf<INoProtocol>(address7)
+                .AndThen(maybe =>
+                {
+                    Assert.False(maybe.IsPresent);
+                    scanResults.ScanFound.WriteUsing("notFoundCount", 1);
+                    return maybe;
+                });
+
+            Assert.Equal(5, scanResults.ScanFound.ReadFrom<int>("foundCount"));
+            Assert.Equal(2, scanResults.ScanFound.ReadFrom<int>("notFoundCount"));
         }
 
         [Fact]
@@ -158,6 +213,31 @@ namespace Vlingo.Actors.Tests
         {
             public static ThreadLocal<TestInterfaceActor> Instance = new ThreadLocal<TestInterfaceActor>();
             public TestInterfaceActor() => Instance.Value = this;
+        }
+
+        private class ScanResult
+        {
+            private readonly AtomicInteger foundCount = new AtomicInteger(0);
+            private readonly AtomicInteger notFoundCount = new AtomicInteger(0);
+            
+            public AccessSafely ScanFound { get; private set; }
+
+            public ScanResult()
+            {
+                ScanFound = AfterCompleting(0);
+            }
+
+            public AccessSafely AfterCompleting(int times)
+            {
+                ScanFound = AccessSafely
+                    .AfterCompleting(times)
+                    .WritingWith("foundCount", (int increment) => foundCount.Set(foundCount.Get() + increment))
+                    .ReadingWith("foundCount", () => foundCount.Get())
+                    .WritingWith("notFoundCount", (int increment) => notFoundCount.Set(notFoundCount.Get() + increment))
+                    .ReadingWith("notFoundCount", () => notFoundCount.Get());
+
+                return ScanFound;
+            }
         }
     }
 }
