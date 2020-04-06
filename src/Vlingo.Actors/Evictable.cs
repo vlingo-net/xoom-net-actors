1740559f91617e2a1b3a3a9ac619298f4cfbf9c9
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-04-06 13:16:07 +0200

    [WIP] Related to #84 and actor eviction

---
+++ src/Vlingo.Actors/Evictable.cs
@@ -0,0 +1,56 @@
+// Copyright © 2012-2020 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using Vlingo.Common;
+
+namespace Vlingo.Actors
+{
+    sealed class Evictable
+    {
+        private Actor _actor;
+        private long _activeOn;
+
+        Evictable(Actor actor)
+        {
+            _actor = actor;
+            _activeOn = DateTimeHelper.CurrentTimeMillis();
+        }
+        
+        void ReceivedMessage() => ActiveOn(DateTimeHelper.CurrentTimeMillis());
+
+        void ActiveOn(long activeOn) => _activeOn = activeOn;
+
+        bool Stop(long thresholdMillis) => Stop(DateTimeHelper.CurrentTimeMillis(), thresholdMillis);
+
+        bool Stop(long referenceMillis, long thresholdMillis)
+        {
+            if (!_actor.Definition.Evictable)
+            {
+                return false;
+            }
+
+            var pendingMessageCount = _actor.LifeCycle.Environment.Mailbox.PendingMessages;
+            if (IsStale(referenceMillis, thresholdMillis))
+            {
+                if (pendingMessageCount == 0)
+                {
+                    _actor.SelfAs<IStoppable>().Stop();
+                    return true;
+                }
+
+                _actor.Logger.Warn(
+                    "Inactive Actor at {} failed to evict because it has {} undelivered messages in its mailbox",
+                    _actor.Address, pendingMessageCount);
+            }
+            return false;
+        }
+
+        bool IsStale(long thresholdMillis) => IsStale(DateTimeHelper.CurrentTimeMillis(), thresholdMillis);
+
+        bool IsStale(long referenceMillis, long thresholdMillis) => _activeOn < referenceMillis - thresholdMillis;
+    }
+}
