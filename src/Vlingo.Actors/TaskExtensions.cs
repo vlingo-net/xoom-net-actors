f26569d9e5ca3cc3a35b4abb01a98d383b17db89
Tomasz Jaskula <thomasv1000@hotmail.fr>
2020-04-26 23:31:59 +0200

    Add ToApm for tasks

--- src/Vlingo.Actors/TaskExtensions.cs
+++ src/Vlingo.Actors/TaskExtensions.cs
@@ -21,11 +21,49 @@ namespace Vlingo.Actors
                 {
                     failure?.Invoke(t.Exception);
                 }
+
                 if (t.IsCompleted)
                 {
                     success(t.Result);
                 }
             }, CancellationToken.None, TaskContinuationOptions.ExecuteSynchronously, TaskScheduler.Default);
+        }
+
+        static Task<TResult> ToApm<TResult>(this Task<TResult> task, AsyncCallback callback, object state)
+        {
+            if (task.AsyncState == state)
+            {
+                if (callback != null)
+                {
+                    task.ContinueWith(delegate { callback(task); },
+                        CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Default);
+                }
+
+                return task;
+            }
+
+            var tcs = new TaskCompletionSource<TResult>(state);
+            task.ContinueWith(delegate
+            {
+                if (task.IsFaulted)
+                {
+                    tcs.TrySetException(task.Exception.InnerExceptions);
+                }
+                else if (task.IsCanceled)
+                {
+                    tcs.TrySetCanceled();
+                }
+                else
+                {
+                    tcs.TrySetResult(task.Result);
+                }
+
+                if (callback != null)
+                {
+                    callback(tcs.Task);
+                }
+            }, CancellationToken.None, TaskContinuationOptions.None, TaskScheduler.Default);
+            return tcs.Task;
         }
     }
 }
