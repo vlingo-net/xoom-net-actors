42119e4b45398d16848ad661314917d618459e52
Tomasz Jaskula <thomasv1000@hotmail.fr>
2022-02-10 22:36:02 +0100

    Define IPluginTypeLoader

---
+++ src/Vlingo.Xoom.Actors/Plugin/IPluginTypeLoader.cs
@@ -0,0 +1,17 @@
+// Copyright © 2012-2022 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.IO;
+
+namespace Vlingo.Xoom.Actors.Plugin;
+
+public interface IPluginTypeLoader
+{
+    Type? LoadType(string name);
+    Stream? LoadResource(string resourceName);
+}
---
+++ src/Vlingo.Xoom.Actors/Plugin/StaticPluginTypeLoader.cs
@@ -0,0 +1,24 @@
+// Copyright © 2012-2022 VLINGO LABS. All rights reserved.
+//
+// This Source Code Form is subject to the terms of the
+// Mozilla Public License, v. 2.0. If a copy of the MPL
+// was not distributed with this file, You can obtain
+// one at https://mozilla.org/MPL/2.0/.
+
+using System;
+using System.IO;
+
+namespace Vlingo.Xoom.Actors.Plugin;
+
+public class PluginTypeLoader : IPluginTypeLoader
+{
+    public Type LoadType(string name)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Stream? LoadResource(string resourceName)
+    {
+        throw new NotImplementedException();
+    }
+}
